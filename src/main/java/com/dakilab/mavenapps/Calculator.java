6494347891f3c420540d0920d3c7ea029a18c26c
Abdelmajid Saadi <saadi.abdelmajid.gmail.com>
2020-10-28 19:44:55 +0100

    fifth commit

--- src/main/java/com/dakilab/mavenapps/Calculator.java
+++ src/main/java/com/dakilab/mavenapps/Calculator.java
@@ -3,6 +3,13 @@ package com.dakilab.mavenapps;
 public class Calculator {
 
     public int calculerSomme(int x, int y){
+        System.out.println("Bonjour");
+        System.out.println("Bonjour");
+        System.out.println("Bonjour");
+        System.out.println("Bonjour");
+        System.out.println("Bonjour");
+        System.out.println("Bonjour");
+        System.out.println("Bonjour");
         return x + y;
     }
 }
